52121a61d36590ffa530d8710b7abbb5cd07223a
nirmalravidas <nirmalravidas.email@gmail.com>
2026-02-01 01:01:30 +0530

    Implement API Gateway using Spring Cloud Gateway, dockerize all microservices, and initialize Kubernetes setup

--- job/src/main/java/com/nirmalravidas/job/clients/CompanyClient.java
+++ job/src/main/java/com/nirmalravidas/job/clients/CompanyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "COMPANY")
+@FeignClient(name = "COMPANY-SERVICE", url = "${company-service.url}")
 public interface CompanyClient {
 
     @GetMapping("/api/companies/{id}")
